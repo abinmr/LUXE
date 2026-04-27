06a1331e4740677ad89549e5338e8d5f32f470a5
Abin <abinmr1617@gmail.com>
2026-04-27 21:27:57 +0530

    created separate checkout controller and some bug fixed and improvements

---
+++ controllers/checkout.controller.js
@@ -0,0 +1,222 @@
+import Address from "../models/address.model.js";
+import Cart from "../models/cart.model.js";
+import Order from "../models/order.model.js";
+import Product from "../models/product.model.js";
+import { calcPricing, getCartItems } from "../service/cart.service.js";
+import { createAddress, findAddresses } from "../service/profile.service.js";
+import { customAlphabet } from "nanoid";
+
+export const getDefaultAddress = async (req, res, next) => {
+    try {
+        const address = await Address.find({ user: req.user?._id });
+        const defaultAddress = address.find((add) => add.isDefault) || address[0];
+        res.locals.defaultAddress = defaultAddress;
+        next();
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const checkoutAddAddress = async (req, res) => {
+    try {
+        const address = await createAddress(req.body, req.user?._id);
+        return res.status(201).json({ success: true, address });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ success: false, message: "Failed to save address" });
+    }
+};
+
+export const getCheckoutPage = async (req, res) => {
+    try {
+        const address = await findAddresses(req.user?._id);
+        const allCartItems = await getCartItems(req.user?._id);
+        const products = allCartItems.filter((item) => item.isSelected);
+        const data = calcPricing(products);
+        const formattedProducts = products.map(({ itemId, description, isSelected, categoryActive, isListed, stock, compareAtPrice, productImage, ...rest }) => ({
+            ...rest,
+            productImage: productImage[0],
+        }));
+        req.session.checkout = {
+            source: "cart",
+            items: formattedProducts,
+            subtotal: data.subtotal,
+            discount: 0,
+            gst: data.gst,
+            shipping: data.shipping,
+            total: data.total,
+        };
+        return res.render("checkout", { address, data, products });
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+export const checkoutBuyNow = async (req, res) => {
+    try {
+        const { productId, variantId, sizeId, quantity } = req.body;
+        if (!productId || !variantId || !sizeId) {
+            req.flash("toast", JSON.stringify({ type: "error", message: "error" }));
+            return res.redirect(`/product/${productId}`);
+        }
+
+        const product = await Product.findById(productId, { name: 1, isListed: 1, isDeleted: 1, variants: 1 });
+        if (!product || !product.isListed) {
+            req.flash("home", { type: "error", message: "product no longer available" });
+            return res.redirect("/home");
+        }
+        const variant = product.variants.id(variantId);
+        const size = variant.sizes.id(sizeId);
+        if (!variant || !size) {
+            req.flash("toast", { type: "error", message: "error" });
+            return res.redirect(`/product/${productId}`);
+        }
+        const stock = size.stock;
+        if (stock === 0) {
+            req.flash("productDetails", { type: "error", message: "out of stock" });
+            return res.redirect(`/product/${productId}`);
+        }
+
+        const products = [
+            {
+                productName: product.name,
+                quantity: quantity,
+                isListed: product.isListed,
+                isSelected: true,
+                price: size.price,
+            },
+        ];
+
+        const data = calcPricing(products);
+
+        const address = await findAddresses(req.user?._id);
+        req.session.checkout = {
+            source: "buy-now",
+            items: [
+                {
+                    productId,
+                    variantId,
+                    sizeId,
+                    productName: product.name,
+                    productImage: variant.images[0],
+                    color: variant.color,
+                    size: size.size,
+                    quantity,
+                    price: size.price,
+                },
+            ],
+            subtotal: data.subtotal,
+            discount: 0,
+            gst: data.gst,
+            shipping: data.shipping,
+            total: data.total,
+        };
+        return res.render("checkout", { address, products, data });
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+export const checkoutPlaceOrder = async (req, res) => {
+    const { addressId, paymentMethod } = req.body;
+    if (!addressId || !paymentMethod) {
+        return res.status(400).json({ success: false, message: "error processing request" });
+    }
+
+    if (paymentMethod !== "cod") {
+        return res.status(400).json({ success: false, message: "Payment method not supported" });
+    }
+
+    const checkout = req.session.checkout;
+    if (!checkout) {
+        return res.status(400).json({ success: false, message: "Session expired" });
+    }
+
+    const address = await Address.findById(addressId);
+    if (!address) {
+        return res.status(400).json({ success: false, message: "Address not found" });
+    }
+
+    const nanoid = customAlphabet("1234567890", 12);
+    const orderId = `ORD-${nanoid()}`;
+
+    const order = await Order.create({
+        userId: req.user?._id,
+        orderId: orderId,
+        username: req.user?.fullname,
+        items: checkout.items,
+        subtotal: checkout.subtotal,
+        discount: checkout.discount,
+        GST: checkout.gst,
+        shipping: checkout.shipping,
+        total: checkout.total,
+        shippingAddress: {
+            fullName: address.fullName,
+            phone: address.phone,
+            pincode: address.pincode,
+            houseNumber: address.houseNumber,
+            street: address?.street,
+            city: address.city,
+            state: address.state,
+        },
+        paymentMethod: paymentMethod,
+        paymentStatus: paymentMethod === "cod" ? "pending" : "paid",
+        orderStatus: "pending",
+        estimatedDeliveryDate: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+    });
+
+    if (order) {
+        for (const item of checkout.items) {
+            const result = await Product.updateOne(
+                {
+                    _id: item.productId,
+                    "variants._id": item.variantId,
+                },
+                {
+                    $inc: {
+                        "variants.$[v].sizes.$[s].stock": -item.quantity,
+                    },
+                },
+                {
+                    arrayFilters: [{ "v._id": item.variantId }, { "s._id": item.sizeId }],
+                },
+            );
+
+            if (result.modifiedCount === 0) {
+                return res.status(400).json({ success: false, message: "error" });
+            }
+        }
+    }
+
+    if (checkout.source === "cart") {
+        const cart = await Cart.findOne({ userId: req.user._id });
+        const seletedIds = cart.items.map((item) => item.sizeId._id.toString());
+        await Cart.updateOne(
+            {
+                userId: req.user._id,
+            },
+            {
+                $pull: {
+                    items: {
+                        isSelected: true,
+                        sizeId: { $in: seletedIds },
+                    },
+                },
+            },
+        );
+    }
+
+    delete req.session.checkout;
+
+    return res.status(200).json({ success: true, order: order._id });
+};
+
+export const getCheckoutSuccessPage = async (req, res) => {
+    const id = req.query.orderId;
+    const order = await Order.findOne({ _id: id });
+    return res.render("checkoutSuccess", { order });
+};
+
+export const checkoutFailurePage = (req, res) => {
+    return res.render("checkoutFail");
+};
